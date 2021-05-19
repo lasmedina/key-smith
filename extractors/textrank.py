1d975684df846f961f77bcb28e22e1a6cf205b1b
lasmedina <lasmedina@gmail.com>
2021-05-19 16:49:30 +0100

    Refactor TextRank pos tags and list indexing

--- extractors/textrank.py
+++ extractors/textrank.py
@@ -1,24 +1,25 @@
 import networkx as nx
 import spacy
-from spacy.matcher import PhraseMatcher
 
 
 class TextRank:
     """."""
 
     def __init__(self):
-        self._nlp = spacy.load("en_core_web_sm")
+        self.nlp = spacy.load("en_core_web_sm")
         self.doc = None
         self.window_n = 10
         self.top_candidates = 6
         self.G = nx.Graph()
 
-    def __find_cooccurrences(self):
+    def __find_cooccurrences(self, pos_tags):
         list_tokens = list(self.doc)
         for i in range(0, len(list_tokens) - self.window_n):
+            token_i = list_tokens[i]
             for j in range(i, i + self.window_n):
-                if list_tokens[i].pos_ in ['ADJ', 'NOUN'] and list_tokens[j].pos_ in ['ADJ', 'NOUN']:
-                    self.G.add_edge(list_tokens[i].text, list_tokens[j].text)
+                token_j = list_tokens[j]
+                if token_i.pos_ in pos_tags and token_j.pos_ in pos_tags:
+                    self.G.add_edge(token_i.text, token_j.text)
 
     def __run_pagerank(self):
         scored_candidates = nx.pagerank(self.G, alpha=0.85, tol=1e-4, max_iter=30)
@@ -33,9 +34,10 @@ class TextRank:
         keyphrase = []
         summed_scores = 0
         for i in range(len(list_tokens)):
-            if list_tokens[i].text in sorted_keys:
-                keyphrase.append(list_tokens[i].text)
-                summed_scores += sorted_candidates[list_tokens[i].text]
+            token_i = list_tokens[i]
+            if token_i.text in sorted_keys:
+                keyphrase.append(token_i.text)
+                summed_scores += sorted_candidates[token_i.text]
             else:
                 if len(keyphrase) > 0:
                     keyphrases[tuple(keyphrase)] = summed_scores
@@ -44,9 +46,11 @@ class TextRank:
 
         return keyphrases
 
-    def extract_keywords(self, text):
-        self.doc = self._nlp(text.lower())
-        self.__find_cooccurrences()
+    def extract_keywords(self, text, pos_tags=None):
+        if pos_tags is None:
+            pos_tags = ['ADJ', 'NOUN']
+        self.doc = self.nlp(text.lower())
+        self.__find_cooccurrences(pos_tags)
         keyphrases = self.__run_pagerank()
 
         sorted_keyphrases = sorted(keyphrases, key=keyphrases.get, reverse=True)
